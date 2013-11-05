8c73c34d8a3af965311a205bdc36ce2c6e1e6080
Stephen Suen <ssuen@mit.edu>
2013-11-04 23:40:05 -0500

    Figured out transforms, updated user tests and added server-side household tests.

---
+++ tests/households.js
@@ -0,0 +1,133 @@
+/* Unit tests for household model and methods - creation and adding or removing
+ * of users. Makes use of the Laika framework (http://arunoda.github.io/laika),
+ * which can simulate both server and client elements. These can emit events (
+ * and any relevant data) in response to changes in the db, and use Node's assert
+ * library to compare actual vs. expected values and catch errors via regex.
+ */
+
+ var assert = require('assert');
+
+ suite('Households, server only -', function() {
+  // creating a new household should add a new record to the db
+  test('household saves to db', function(done, server) {
+    server.eval(function() {
+      Households.insert({name: 'MIT'});
+      var docs = Households.find().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.length, 1);
+      done();
+    });
+  });
+
+  // creating multiple households should add that many records to the db
+  test('multiple households can be created', function(done, server) {
+    server.eval(function() {
+      Households.insert({name: 'MIT'});
+      Households.insert({name: 'Harvard'});
+      Households.insert({name: 'Caltech'});
+      var docs = Households.find().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.length, 3);
+      done();
+    });
+  });
+
+  // adding a user to a household should update the db
+  test('adding user to household saves to db', function(done, server) {
+    server.eval(function() {
+      Accounts.createUser({email: 'user@mit.edu', password: 'security'});
+      Households.insert({name: 'MIT'});
+
+      var user = Meteor.users.findOne();
+      var household = Households.findOne();
+
+      Households.update({_id: household._id}, {$addToSet: {user_ids: user._id}});
+
+      var docs = user.households().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs[0].name, 'MIT');
+      done();
+    });
+  });
+
+  // adding multiple users to a household should be supported
+  test('adding multiple users to one household', function(done, server) {
+    server.eval(function() {
+      Accounts.createUser({email: 'user1@mit.edu', password: 'security'});
+      Accounts.createUser({email: 'user2@mit.edu', password: 'password'});
+      Accounts.createUser({email: 'user3@mit.edu', password: 'sosecure'});
+      Households.insert({name: 'MIT'});
+
+      var users = Meteor.users.find();
+      var household = Households.findOne();
+
+      users.forEach(function(user) {
+        Households.update({_id: household._id}, {$addToSet: {user_ids: user._id}});
+      });
+
+      var docs = Households.findOne();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.user_ids.length, 3);
+      done();
+    });
+  });
+
+  // adding a user to multiple households should be supported
+  test('adding one user to multiple households', function(done, server) {
+    server.eval(function() {
+      Accounts.createUser({email: 'user@mit.edu', password: 'password'});
+      Households.insert({name: 'Kappa Alpha Theta'});
+      Households.insert({name: 'Phi Sigma Kappa'});
+      Households.insert({name: 'East Campus'});
+
+      var user = Meteor.users.findOne();
+      var households = Households.find();
+
+      households.forEach(function(household) {
+        Households.update({_id: household._id}, {$addToSet: {user_ids: user._id}});
+      });
+
+      var docs = Meteor.users.findOne().households().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.length, 3);
+      done();
+    });
+  });
+
+  // removing a user from a household should update the db
+  test('removing user from household updates db', function(done, server) {
+    server.eval(function() {
+      Accounts.createUser({email: 'user@mit.edu', password: 'password'});
+      Households.insert({name: 'MIT'});
+
+      var user = Meteor.users.findOne();
+      var household = Households.findOne();
+
+      Households.update({_id: household._id}, {$addToSet: {user_ids: user._id}});
+      Households.update({_id: household._id}, {$pull: {user_ids: user._id}});
+
+      var docs = Households.find().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs[0].user_ids.length, 0);
+      done();
+    });
+  });
+});
--- tests/users.js
+++ tests/users.js
@@ -1,135 +1,196 @@
-/* Unit tests for user model and methods - registration, login, authorization.
- * Makes use of the Laika framework (http://arunoda.github.io/laika), which
- * can simulate both server and client elements. These can emit events (and
- * any relevant data) in response to changes in the db, and use Node's assert
- * library to compare actual vs. expected values and catch errors via regex.
- */
-var assert = require('assert');
+// /* Unit tests for user model and methods - registration, login, authorization.
+//  * Makes use of the Laika framework (http://arunoda.github.io/laika), which
+//  * can simulate both server and client elements. These can emit events (and
+//  * any relevant data) in response to changes in the db, and use Node's assert
+//  * library to compare actual vs. expected values and catch errors via regex.
+//  */
+// var assert = require('assert');
 
-suite('Server only -', function() {
-  // registering a new account should add a new record to the db
-  test('account registers to Meteor.users', function(done, server) {
-    server.eval(function() {
-      Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
-      var docs = Meteor.users.find().fetch();
-      emit('docs', docs);
-    });
+// suite('Users, server only -', function() {
+//   // registering a new account should add a new record to the db
+//   test('account registers to Meteor.users', function(done, server) {
+//     server.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
+//       var docs = Meteor.users.find().fetch();
+//       emit('docs', docs);
+//     });
 
-    server.once('docs', function(docs) {
-      assert.equal(docs.length, 1);
-      done();
-    });
-  });
+//     server.once('docs', function(docs) {
+//       assert.equal(docs.length, 1);
+//       done();
+//     });
+//   });
 
-  // registering multiple accounts should add that many records to the db
-  test('multiple accounts can be registered', function(done, server) {
-    server.eval(function() {
-      Accounts.createUser({email: 'user1@mit.edu', password: 'security'});
-      Accounts.createUser({email: 'user2@mit.edu', password: 'airtight'});
-      Accounts.createUser({email: 'user3@mit.edu', password: 'password'});
-      var docs = Meteor.users.find().fetch();
-      emit('docs', docs);
-    });
+//   // registering multiple accounts should add that many records to the db
+//   test('multiple accounts can be registered', function(done, server) {
+//     server.eval(function() {
+//       Accounts.createUser({email: 'user1@mit.edu', password: 'security'});
+//       Accounts.createUser({email: 'user2@mit.edu', password: 'airtight'});
+//       Accounts.createUser({email: 'user3@mit.edu', password: 'password'});
+//       var docs = Meteor.users.find().fetch();
+//       emit('docs', docs);
+//     });
 
-    server.once('docs', function(docs) {
-      assert.equal(docs.length, 3);
-      done();
-    });
-  });
+//     server.once('docs', function(docs) {
+//       assert.equal(docs.length, 3);
+//       done();
+//     });
+//   });
 
-  // a user record should retain the email it was registered with
-  test('email param gets saved to db', function(done, server) {
-    server.eval(function() {
-      Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
-      var docs = Meteor.users.findOne();
-      emit('docs', docs);
-    });
+//   // a user record should retain the email it was registered with
+//   test('email param gets saved to db', function(done, server) {
+//     server.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
+//       var docs = Meteor.users.findOne();
+//       emit('docs', docs);
+//     });
 
-    server.once('docs', function(docs) {
-      assert.equal(docs.emails[0].address, 'user@mit.edu');
-      done();
-    });
-  });
+//     server.once('docs', function(docs) {
+//       assert.equal(docs.emails[0].address, 'user@mit.edu');
+//       done();
+//     });
+//   });
 
-  // there should be a one-to-one relationship between users and emails
-  test('only allow one account per email', function(done, server) {
-    var error = server.evalSyncExpectError('failed signup', function() {
-      Accounts.createUser({email: 'user@mit.edu', password: 'account1'});
-      Accounts.createUser({email: 'user@mit.edu', password: 'account2'});
-      emit('return');
-    });
+//   // a user email should be accessible via the primary_email method
+//   test('email param accessible via primary_email', function(done, server) {
+//     server.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
+//       var docs = Meteor.users.findOne().primary_email();
+//       emit('docs', docs);
+//     });
 
-    assert.ok(error.message.match(/Email already exists/));
-    done();
-  });
+//     server.once('docs', function(docs) {
+//       assert.equal(docs, 'user@mit.edu');
+//       done();
+//     });
+//   });
 
-  // TODO: test methods from transforming the collection
-});
+//   // there should be a one-to-one relationship between users and emails
+//   test('only allow one account per email', function(done, server) {
+//     var error = server.evalSyncExpectError('failed signup', function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'account1'});
+//       Accounts.createUser({email: 'user@mit.edu', password: 'account2'});
+//       emit('return');
+//     });
 
-suite('Server + client -', function() {
-  // registering a new account should also work from the client side
-  test('account registers to Meteor.users (client-side)', function(done, server, client) {
-    server.eval(function() {
-      Meteor.users.find().observe({
-        added: function(doc) {
-          emit('added', doc);
-        }
-      });
-    }).once('added', function(doc) {
-      assert.equal(doc.emails[0].address, 'user@mit.edu');
-      done();
-    });
+//     assert.ok(error.message.match(/Email already exists/));
+//     done();
+//   });
+// });
 
-    client.eval(function() {
-      Accounts.createUser({email: 'user@mit.edu', password: 'password'});
-    });
-  });
+// suite('Users, server + client -', function() {
+//   // registering a new account should also work from the client side
+//   test('account registers to Meteor.users', function(done, server, client) {
+//     server.eval(function() {
+//       Meteor.users.find().observe({
+//         added: function(doc) {
+//           emit('added', doc);
+//         }
+//       });
+//     }).once('added', function(doc) {
+//       assert.equal(doc.emails[0].address, 'user@mit.edu');
+//       done();
+//     });
 
-  // TODO: an invalid log in should return an error
-  // (I've tried everything here, to no avail - will come back later)
+//     client.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'password'});
+//     });
+//   });
 
-  // a registered user should be able to log in and create a household
-  test('registered user can log in and create a household', function(done, server, client) {
-    server.eval(function() {
-      Accounts.createUser({email: 'user@mit.edu', password: 'password'});
-      emit('done');
-    }).once('done', function() {
-      server.eval(function() {
-        Households.find().observe({
-          added: function(doc) {
-            emit('added', doc);
-          }
-        });
-      });
-    });
+//   test('multiple accounts can be registered', function(done, client) {
+//     client.eval(function() {
+//       Accounts.createUser({email: 'user1@mit.edu', password: 'security'});
+//       Accounts.createUser({email: 'user2@mit.edu', password: 'airtight'});
+//       Accounts.createUser({email: 'user3@mit.edu', password: 'password'});
+//       var docs = Meteor.users.find().fetch();
+//       emit('docs', docs);
+//     });
 
-    server.once('added', function(doc) {
-      assert.equal(doc.name, 'MIT');
-      done();
-    });
+//     client.once('docs', function(docs) {
+//       assert.equal(docs.length, 3);
+//       done();
+//     });
+//   });
 
-    client.eval(function() {
-      Meteor.loginWithPassword('user@mit.edu', 'password', function() {
-        Households.insert({name: 'MIT'}); // this is a stub - we're just checking authorization
-      });
-    });
-  });
+//   test('email param gets saved to db', function(done, client) {
+//     client.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
+//       var docs = Meteor.users.findOne();
+//       emit('docs', docs);
+//     });
 
-  // an unregistered user should be unable to create a household
-  test('unregistered user cannot make a household', function(done, server, client) {
-    client.eval(function() {
-      Households.find().observe({
-        removed: function(doc) { // unauthorized households are instantly removed
-          emit('remove', doc);
-        }
-      });
+//     client.once('docs', function(docs) {
+//       assert.equal(docs.emails[0].address, 'user@mit.edu');
+//       done();
+//     });
+//   });
 
-      Households.insert({name: 'Harvard'}); // this is a stub - we're just checking authorization
-    });
+//   test('email param accessible via primary_email', function(done, client) {
+//     client.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'sosecure'});
+//       var docs = Meteor.users.findOne().primary_email();
+//       emit('docs', docs);
+//     });
 
-    client.once('remove', function(doc) {
-      assert.equal(doc.name, 'Harvard');
-      done();
-    });
-  });
-});
+//     client.once('docs', function(docs) {
+//       assert.equal(docs, 'user@mit.edu');
+//       done();
+//     });
+//   });
+
+//   test('only allow one account per email', function(done, client) {
+//     var error = client.evalSyncExpectError('failed signup', function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'account1'});
+//       Accounts.createUser({email: 'user@mit.edu', password: 'account2'});
+//       emit('return');
+//     });
+
+//     assert.ok(error.message.match(/Email already exists/));
+//     done();
+//   });
+
+//   // a registered user should be able to log in and create a household
+//   test('registered user can log in and create a household', function(done, server, client) {
+//     server.eval(function() {
+//       Accounts.createUser({email: 'user@mit.edu', password: 'password'});
+//       emit('done');
+//     }).once('done', function() {
+//       server.eval(function() {
+//         Households.find().observe({
+//           added: function(doc) {
+//             emit('added', doc);
+//           }
+//         });
+//       });
+//     });
+
+//     server.once('added', function(doc) {
+//       assert.equal(doc.name, 'MIT');
+//       done();
+//     });
+
+//     client.eval(function() {
+//       Meteor.loginWithPassword('user@mit.edu', 'password', function() {
+//         Households.insert({name: 'MIT'}); // this is a stub - we're just checking authorization
+//       });
+//     });
+//   });
+
+//   // an unregistered user should be unable to create a household
+//   test('unregistered user cannot make a household', function(done, server, client) {
+//     client.eval(function() {
+//       Households.find().observe({
+//         removed: function(doc) { // unauthorized households are removed automatically
+//           emit('remove', doc);
+//         }
+//       });
+
+//       Households.insert({name: 'Harvard'}); // this is a stub - we're just checking authorization
+//     });
+
+//     client.once('remove', function(doc) {
+//       assert.equal(doc.name, 'Harvard');
+//       done();
+//     });
+//   });
+// });
